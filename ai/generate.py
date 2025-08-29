f57642e2b72fd1b7c850cc87be3c3eb47f7c8ca5
VladWLSTR <user@WLMB134-KP4HGXLJXD.local>
2025-08-29 12:59:27 +0300

    kk

--- ai/generate.py
+++ ai/generate.py
@@ -3,7 +3,7 @@ from config import AI_TOKEN
 
 client = AsyncOpenAI(
   base_url="https://openrouter.ai/api/v1",
-  api_key=AI_TOKEN,
+  api_key=AI_TOKEN.strip(),
 )
 
 async def ai_generate(text: str):
