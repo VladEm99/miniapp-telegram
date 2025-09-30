c5f4fd19b8b37f983cf4c371e7156ed2545e2819
VladWLSTR <user@dhcp-4-229.wallester>
2025-09-30 10:02:51 +0300

    security commit

--- run.py
+++ run.py
@@ -1,12 +1,11 @@
 from aiogram import Bot, Dispatcher
 import asyncio
-from config import TG_TOKEN
-
+import os
 from ai.handlers import router
 
 
 async def main():
-    bot = Bot(token=TG_TOKEN)
+    bot = Bot(token=os.environ["TELEGRAM_BOT_TOKEN"].strip())
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
